dca49f7bc78450582f172db43033b73458e0a67b
JasperBleeker <jasper.bleeker@stud.fhgr.ch>
2025-07-18 13:23:56 +0200

    Minor Adjustments
    Some styling Adjustments
    - fixed scaling of models:
       - Blender
       -Clock
       -Monstera

--- AR-Commerce/src/lib/components/IndexHeader.svelte
+++ AR-Commerce/src/lib/components/IndexHeader.svelte
@@ -1,5 +1,5 @@
-<header class="bg-[#F2F2F2] text-[#2D2D2D] px-4 pt-20 pb-8 md:py-16 flex flex-col items-start justify-between shadow-md">
-  <div class="w-full max-w-3xl mx-auto">
+<header class="bg-[#F2F2F2] text-[#2D2D2D] px-4 pt-20 pb-8 flex flex-col items-start justify-between shadow-md">
+  <div class="w-full max-w-3xl md:max-w-xl mx-auto lg:max-w-3xl">
     <h1 class="text-3xl font-black sm:text-4xl md:text-5xl mb-8 text-left">Willkommen zu AR-Commerce</h1>
     <p class="text-base sm:text-lg md:text-xl text-left">
       AR-Commerce ist eine Plattform, die es ermöglicht, Produkte in der realen Welt zu visualisieren und zu testen.
