f5d3dbacd46628f4b8ee4884bb8d5116d6d4baaa
JasperBleeker <jasper.bleeker@stud.fhgr.ch>
2025-07-19 13:07:27 +0200

    Node Docker deployment
    Made application ready for first deployment

--- AR-Commerce/src/lib/server/mongo.ts
+++ AR-Commerce/src/lib/server/mongo.ts
@@ -1,13 +1,22 @@
 import { MongoClient } from 'mongodb';
 import { MONGODB_URI } from '$env/static/private';
+import fs from 'fs';
+
+// Prefer Docker secret if available, otherwise use environment variable
+let uri = MONGODB_URI;
+try {
+  uri = fs.readFileSync('/run/secrets/mongo_uri', 'utf8').trim();
+} catch (err) {
+  // Ignore if file does not exist
+}
+
+if (!uri) {
+  throw new Error('Please define the MONGODB_URI environment variable or Docker secret');
+}
+
 declare global {
   // eslint-disable-next-line no-var
   var _mongoClientPromise: Promise<MongoClient> | undefined;
-}
-
-const uri = MONGODB_URI;
-if (!uri) {
-  throw new Error('Please define the MONGODB_URI environment variable');
 }
 
 let client: MongoClient;
